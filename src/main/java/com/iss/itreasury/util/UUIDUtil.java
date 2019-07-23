0e1110238e98a3f70a9362d62cc9c5878b996353
1061199028@qq.com <1061199028@qq.com>
2019-07-23 14:27:36 +0800

    修改

--- src/main/java/com/iss/itreasury/util/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package com.iss.itreasury.util;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-	public static String creatUUID() {
-		return UUID.randomUUID().toString().replace("-", "");
-	}
-}
